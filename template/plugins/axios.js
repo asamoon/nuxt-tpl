46e778c3ca764a74b5ac13738b1bdf4a78ba1fd3
asamoon <asamoon@gmail.com>
2017-07-10 04:34:59 +0800

    fix

--- template/plugins/axios.js
+++ template/plugins/axios.js
@@ -2,13 +2,13 @@
 * @Author: asamoon
 * @Date:   2017-05-10 15:48:15
 * @Last Modified by:   asamoon
-* @Last Modified time: 2017-07-10 02:43:40
+* @Last Modified time: 2017-07-10 04:34:34
 */
 
 import axios from 'axios'
 
 export default axios.create({
-  // baseURL: 'https://voteage-demo-01.firebaseio.com'
+  // baseURL: ''
   headers: {
     'Content-Type': 'application/x-www-form-urlencoded'
   },
